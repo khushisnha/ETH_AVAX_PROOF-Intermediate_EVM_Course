8e636eaa530bedad92f3ed5ae763fc8ef4d170a5
Khushi <137313256+khushisnha@users.noreply.github.com>
2023-07-12 16:49:27 +0530

    hardhat.js

---
+++ MODULE-3_FINAL PROJECT/hardhat.js
@@ -0,0 +1,7 @@
+npm install @openzeppelin/contracts    //installing OpenZeppelin
+
+npm install -- save-dev hardhat    //installing hardhat
+
+npm hardhat compile    //compiling through hardhat
+
+npm hardhat node    //for getting addresses and keys
