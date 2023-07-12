9692a4c444c28f5653991104537c19f33fd4f9b9
Khushi <137313256+khushisnha@users.noreply.github.com>
2023-07-12 16:25:39 +0530

    contract

--- MODULE-3_FINAL PROJECT/contract.sol
+++ MODULE-3_FINAL PROJECT/contract.sol
@@ -3,24 +3,29 @@ Write a smart contract to create your own token on a local HardHat network. Once
 the contract owner should be able to mint tokens to a provided address. Any user should be able to burn and transfer tokens.
 */
 
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT    
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.0; 
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Myether is ERC20 {
+
     address private owner;
 
-    constructor() ERC20("Khushi", "kk") {
+    constructor() ERC20("Khushi", "kk")
+    {
         owner = msg.sender;
     }
 
-    modifier onlyOwner() {
+    modifier onlyOwner()
+    {
         require(msg.sender == owner, "Only the contract owner has rights over this function!");
         _;
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public onlyOwner
+    {
         _mint(to, amount);
     }
 
@@ -29,8 +34,11 @@ contract Myether is ERC20 {
     }
 
     function transfer(address to, uint256 amount) public override returns (bool) {
+
         require(amount <= balanceOf(msg.sender), "Not enough balance to Transfer!");
+
         _transfer(msg.sender, to, amount);
+
         return true;
     }
 }
