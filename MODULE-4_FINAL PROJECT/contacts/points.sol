dc5a6c3e242f468efef03c549bb99c99f325b578
Khushi <137313256+khushisnha@users.noreply.github.com>
2023-07-18 19:13:53 +0530

    Update points.sol

--- MODULE-4_FINAL PROJECT/contacts/points.sol
+++ MODULE-4_FINAL PROJECT/contacts/points.sol
@@ -8,29 +8,54 @@ import "hardhat/console.sol";
 
 contract GT is ERC20, Ownable, ERC20Burnable 
 {
+    event LogMessage(string message);
     constructor() ERC20("Degen", "DGN") {}
     
+        //Minting new tokens
         function mint(address to, uint amount) public onlyOwner
         {
             _mint(to, amount);
         }
 
-        function getBalance() external view returns (uint256)
-        {
-            return this.balanceOf(msg.sender);
-        }
-
-        function transferTokens(address _receiver, uint _value) external 
+        //Transferring tokens
+         function transferTokens(address _receiver, uint _value) external 
         {
             require(balanceOf(msg.sender) >= _value, "INSUFFICIENT TOKENS!!");
             approve(msg.sender, _value);
             transferFrom(msg.sender, _receiver, _value);
         }
 
+        //Redeeming tokens
+        function redeemForItem(uint256 itemNo) public {
+        uint256 amount;
+        
+        if (itemNo == 1) {
+            amount = 1;
+        } else if (itemNo == 2) {
+            amount = 2;
+        } else if (itemNo == 3) {
+            amount = 3;
+        } else {
+            emit LogMessage("Redemption was UN-Successful");
+            return;
+        }
+
+        _burn(msg.sender, amount);
+        emit LogMessage("Redemption is Successful");
+    }
+
+        //Checking token balance
+        function getBalance() external view returns (uint256)
+        {
+            return this.balanceOf(msg.sender);
+        }
+
+        //Burning tokens
         function burnTokens(uint256 _value) external 
         {
             require(balanceOf(msg.sender) >= _value, "INSUFFICIENT TOKENS!!");
             _burn(msg.sender, _value);
+            emit LogMessage("Tokens burned successfully");
         }
 
         function decimals() override public pure returns (uint8)
